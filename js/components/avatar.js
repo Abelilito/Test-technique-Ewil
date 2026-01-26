7c048325ff023c24d6012ad8bc3ae1f66fc6c12d
Abel Ilito <ilitoabel@gmail.com>
2026-01-26 14:18:00 +0100

    Feat: add variant on avatar component

--- js/components/avatar.js
+++ js/components/avatar.js
@@ -10,14 +10,58 @@ class AvatarComponent extends HTMLElement {
 
   render() {
     const url = this.getAttribute("img-url");
+    const variant = this.getAttribute("variant" || "default");
+    let className;
+
+    if (variant === "column") {
+      className = "column"
+    } else {
+      className = "default"
+    }
 
     this.shadowRoot.innerHTML = `
       <style>
         img {
           border-radius: 1000px;
         }
+
+        .avatar-component {
+          display: flex;
+          gap: 1rem;
+        }
+
+        .default {
+          align-items: center;
+        }
+
+        .column {
+          flex-direction: column;
+          width: max-content;
+          justify-content: center;
+        }
+
+        .column div {
+          text-align: center;
+        }
+
+        .column img {
+          widht: 6.2rem;
+          height: 6.2rem;
+        }
+
+        .column img {
+          widht: 4rem;
+          height: 4rem;
+          border: 3px solid white;
+        }
       </style>
-      <img src="${url}" alt="Users picture" />
+      
+      <div class="avatar-component ${className}">
+        <img src="${url}" alt="Users picture" />
+        <div>
+          texte
+        </div>
+      </div>
     `;
   }
 }
